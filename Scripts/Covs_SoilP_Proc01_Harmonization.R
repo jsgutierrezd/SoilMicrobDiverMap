0b9ae06b3759efaf05b9559fee12b93b9222f1bd
Sebastian Gutierrez <jsgutierrezd@unal.edu.co>
2023-05-25 15:20:51 +0200

    Covariates harmonization

---
+++ Scripts/Covs_SoilP_Proc01_Harmonization.R
@@ -0,0 +1,11 @@
+#===============================================================
+# Section: Covariates
+# Group: Soil properties
+# Procedure: 01
+# Description: Harmonization of soil properties-related 
+#              covariates
+# Output: Set of sil properties-related covariates harmonized
+#         at 10 m resolution
+# Produced by Sebastian Gutierrez - Aarhus University
+# May 2023
+#===============================================================
