400579b6a67b4ce3ebe38cdfa4f0515fde346cad
Sebastian Gutierrez <jsgutierrezd@unal.edu.co>
2023-06-02 10:55:04 +0200

    Covariates harmonization

--- Scripts/Covs_Mang_Proc06_Aggregate_Rasters_KgNha.R
+++ Scripts/Covs_Mang_Proc06_Aggregate_Rasters_KgNha.R
@@ -18,7 +18,6 @@ Sys.setenv(language="EN")
 
 setwd("~/AARHUS_PhD/DSMactivities/2_Biodiversity/SoilMicrobDiverMap")
 
-
 # 2) Load libraries -------------------------------------------------------
 pckg <- c('terra',
           'tidyverse',
@@ -32,13 +31,47 @@ usePackage <- function(p) {
 }
 lapply(pckg,usePackage)
 
-# 3) Load Geopackage files ------------------------------------------------
-files <- list.files("EnvironmentalLayers/Fertilizer/NLESdata_fields_sp/",
-                    pattern = "shortened_KgNkm2.gpkg$",
-                    full.names = TRUE
-)
-files
+# 3) Load raster files ----------------------------------------------------
 
-fert1721sp <- lapply(files,function(x){
-  vect(x) 
-})
+filenames <- c("commer_fert","livestock_man","other_org_fert","cattle_slur","pig_slur","mink_slur",
+               "soild_livest_man","liquid_livest_man","deep_litter","degassed_biom","sewage_sludge")
+# i=1
+layers <- list()
+for (i in 1:length(filenames)) {
+  files <- list.files("O:/Tech_AGRO/Jord/Sebastian/SoilMicrobialDiversity/Layers10m/Nfert/",
+                      pattern = paste0(filenames[i]),
+                      full.names = TRUE
+  )
+  files <- rast(files)
+  layers[[i]] <- files 
+  
+}
+
+layers
+
+# 4) Aggregate variables using mean and sd --------------------------------
+
+# Multiannual average
+meanlayers <- rast()
+# i=1
+for (i in 1:length(filenames)) {
+  tmp <- mean(layers[[i]]) %>% round(digits = 2)
+  meanlayers <- c(meanlayers,tmp)
+  }
+names(meanlayers) <- paste0(filenames,"_mean")
+writeRaster(meanlayers,filename = paste0("O:/Tech_AGRO/Jord/Sebastian/SoilMicrobialDiversity/Layers10m/Nfert/AggregatedLayers/",
+                                     names(meanlayers),".tif"),overwrite=T)
+
+# Multiannual standard deviation
+sdlayers <- rast()
+for (i in 1:length(filenames)) {
+  tmp <- stdev(layers[[i]]) %>% round(digits = 2)
+  sdlayers <- c(sdlayers,tmp)
+}
+names(sdlayers) <- paste0(filenames,"_sd")
+writeRaster(sdlayers,filename = paste0("O:/Tech_AGRO/Jord/Sebastian/SoilMicrobialDiversity/Layers10m/Nfert/AggregatedLayers/",
+                                         names(sdlayers),".tif"),overwrite=T)
+
+# END ---------------------------------------------------------------------
+
+
