bd69796a7abd331911a3fb04061b36d5cec66073
Sebastian Gutierrez <jsgutierrezd@unal.edu.co>
2023-06-16 14:49:14 +0200

    Filtering Step

---
+++ Study01/Scripts/Dat_Proc02_MultivCovsSelection.R
@@ -0,0 +1,69 @@
+#===============================================================
+# Section: Point dataset of soil microbial diversity
+# Group: Covariates selection
+# Procedure: 02
+# Description: Read the dataset previously filtered using 
+#              univariate methods and use wrapper (multivariate)
+#              methods to find the optimal combination of 
+#              environmental layers to maximize the model 
+#              performance of the target variables
+# Output: The final dataset of predictors that we are using to 
+#         model the target variables.
+# Modified by Sebastian Gutierrez - Aarhus University
+# June 2023
+#===============================================================
+
+rm(list = ls())
+Sys.setenv(language="EN")
+
+# 1) Set working directory ------------------------------------------------
+
+setwd("~/AARHUS_PhD/DSMactivities/2_Biodiversity/SoilMicrobDiverMap")
+
+
+# 2) Load libraries -------------------------------------------------------
+pckg <- c('magrittr',
+          'readr',
+          'caret')
+
+usePackage <- function(p) {
+  if (!is.element(p, installed.packages()[,1]))
+    install.packages(p, dep = TRUE)
+  require(p, character.only = TRUE)
+}
+lapply(pckg,usePackage)
+
+# 3) Load Excel table -----------------------------------------------------
+data <- read_csv("C:/Users/au704633/OneDrive - Aarhus Universitet/Documents/AARHUS_PhD/DSMactivities/2_Biodiversity/Metadata/RegMatStudy1Filtered.csv")
+
+
+
+
+library(mlbench)
+n <- 100
+p <- 40
+sigma <- 1
+set.seed(1)
+sim <- mlbench.friedman1(n, sd = sigma)
+colnames(sim$x) <- c(paste("real", 1:5, sep = ""),
+                     paste("bogus", 1:5, sep = ""))
+bogus <- matrix(rnorm(n * p), nrow = n)
+colnames(bogus) <- paste("bogus", 5+(1:ncol(bogus)), sep = "")
+x <- cbind(sim$x, bogus)
+y <- sim$y
+normalization <- preProcess(x)
+x <- predict(normalization, x)
+x <- as.data.frame(x)
+
+x
+y
+
+
+ga_ctrl <- gafsControl(functions = rfGA,
+                       method = "repeatedcv",
+                       repeats = 5)
+set.seed(10)
+rf_ga <- gafs(x = x, y = y,
+              iters = 200,
+              gafsControl = ga_ctrl)
+rf_ga
